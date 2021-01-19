a6aa68fa245380db44564923673b60d78a770364
Artem Sedov <artysed@gmail.com>
2021-01-19 22:56:17 +0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>ARTYSED.COM</h1>
+    <a href="https://www.linkedin.com/in/artysed/"><img src="LI-Logo.png" alt="LinkedIn" width="140"></a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
